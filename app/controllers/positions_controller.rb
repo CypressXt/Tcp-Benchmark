4436618f7b78f958c47f68162b6bde550a372b8e
Clément Hampaï <clement.hampai@cypressxt.net>
2018-11-26 21:08:34 +0100

    create new positions insted of update

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,18 +1,22 @@
 class PositionsController < ApplicationController
   before_action :get_last, only: [:last, :more_x, :more_y]
+  skip_before_action :verify_authenticity_token
+
 
   def last
     render :json => @position
   end
 
   def more_x
-    @position.x = @position.x + 1
-    render :json => @position.save
+    position = Position.new
+    position.x = @position.x + 1
+    render :json => position.save
   end
 
   def more_y
-    @position.y = @position.y + 1
-    render :json => @position.save
+    position = Position.new
+    position.y = @position.y + 1
+    render :json => position.save
   end
 
   def get_last
