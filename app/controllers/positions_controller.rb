1bd7d524a55ae8df9ed7171befbf0718ff1a864b
Clément Hampaï <clement.hampai@cypressxt.net>
2018-11-26 19:47:52 +0100

    Added position api calls

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,14 +1,28 @@
 class PositionsController < ApplicationController
+  before_action :get_last, only: [:last, :more_x, :more_y]
 
   def last
-    position = Position.last
-    render :json => position
+    render :json => @position
   end
 
   def more_x
+    @position.x = @position.x + 1
+    render :json => @position.save
   end
 
   def more_y
+    @position.y = @position.y + 1
+    render :json => @position.save
+  end
+
+  def get_last
+    @position = Position.last
+    if !@position
+      @position = Position.new
+      @position.x = 0
+      @position.y = 0
+      @position.save
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'positions/last' => "positions#last"
   post 'positions/more_x' => "positions#more_x"
   post 'positions/more_y' => "positions#more_y"
-
-
 
 end
